dd70d202dbe8e2c7ae44202b20895eaddbf501d7
Sachithra Dangalla <sachithradangalla@gmail.com>
2016-08-17 11:09:18 +0530

    Removed unnecessary main method

--- MicrocontrollersGUI/src/microcontrollersgui/MicrocontrollersGUI.java
+++ MicrocontrollersGUI/src/microcontrollersgui/MicrocontrollersGUI.java
@@ -14,7 +14,7 @@ public class MicrocontrollersGUI {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void mainn(String[] args) {
         // TODO code application logic here
     }
     
